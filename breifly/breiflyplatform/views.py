0dc67ce0f15666dee5126dd64f9f839bd1d44e01
Joan Mas Castella <33759606+Joanmascastella@users.noreply.github.com>
2025-01-16 12:30:18 +0900

    login form done

--- breifly/breiflyplatform/views.py
+++ breifly/breiflyplatform/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
+
+
 def landing_page(request):
     context = {
         'title': 'Briefly - Home',
         'user_authenticated': request.user.is_authenticated,
 
     }
+
     return render(request, 'header.html', context)
+
